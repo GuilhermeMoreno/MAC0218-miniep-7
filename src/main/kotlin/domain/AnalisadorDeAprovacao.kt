23e3b0281ee02aad3533426fc6f5b4eea1938991
Guilherme Moreno <guilhermewr9@gmail.com>
2022-07-17 22:05:35 -0300

    Adiciona implementação de AnalisadorDeAprovacao

--- src/main/kotlin/domain/AnalisadorDeAprovacao.kt
+++ src/main/kotlin/domain/AnalisadorDeAprovacao.kt
@@ -1,15 +1,24 @@
 package domain
 
+import domain.Boletim
+import domain.criterios.CriterioDeAprovacao
+
 class AnalisadorDeAprovacao {
 
-    // ---------------------------------
-    //
-    //      Seu código deve vir aqui
-    //
-    //      Defina atributos e métodos conforme especificado
-    //      no arquivo de teste encontrado em
-    //      'src/test/kotlin/domain/AnalisadorDeAprovacaoTest'
-    //
-    // ---------------------------------
+	private lateinit var criterioAtual : CriterioDeAprovacao
 
+	// Configura critério a ser usado
+	fun defineCriterio(criterio : CriterioDeAprovacao) {
+		criterioAtual = criterio
+	}
+
+	// Gera o boletim fechado
+	fun fechaBoletim(boletim : Boletim) : BoletimFechado{
+		return BoletimFechado(
+			mediaEPs = boletim.mediaEPs,
+            mediaMiniEPs = boletim.mediaMiniEPs,			
+            mediaFinal = criterioAtual.mediaFinal(boletim),
+			foiAprovado = criterioAtual.estaAprovado(boletim)
+        )
+	}
 }
